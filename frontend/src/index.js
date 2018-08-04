13078ede2826eb66f1d6cfd689180204ab0420f1
andiwinata <andi.winata@hotmail.com>
2018-08-04 20:06:47 +1000

    Changing page to info page and adding device data form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './pages/index';
+import InfoPage from './pages/InfoPage'
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+// ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<InfoPage />, document.getElementById('root'));
---
+++ frontend/src/pages/InfoPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import DeviceData from './DeviceData';
+
+const styles = theme => ({
+  card: {
+    margin: 20,
+  },
+  paper: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  formButton: {
+    marginTop: theme.spacing.unit,
+    width: '100%',
+  },
+  pre: {
+    background: '#ccc',
+    padding: 10,
+    marginBottom: 0,
+  },
+});
+
+const InfoPage = ({ classes }) => (
+  <div>
+    <Paper className={classes.paper}>
+      <DeviceData />
+    </Paper>
+  </div>
+);
+
+export default withStyles(styles)(InfoPage);
