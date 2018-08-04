905d880908cc24a638536b1edfbcbe358f3fb20a
Kogulan Ledanan <kogulan@isentia.com>
2018-08-05 09:50:50 +1000

    added markers from device location generation simulation

--- frontend/src/pages/InfoPage.js
+++ frontend/src/pages/InfoPage.js
@@ -4,6 +4,13 @@ import Paper from '@material-ui/core/Paper';
 import DeviceData from './DeviceData';
 import Map from './Map.js'
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+
 const styles = theme => ({
   card: {
     margin: 20,
@@ -26,6 +33,16 @@ const styles = theme => ({
 
 const InfoPage = ({ classes }) => (
   <div>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+
+        </IconButton>
+        <Typography variant="title" color="inherit" className={classes.flex}>
+          H2EOS
+        </Typography>
+      </Toolbar>
+    </AppBar>
     <Paper className={classes.paper}>
       <DeviceData />
     </Paper>
--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -7,6 +7,7 @@ import {
 } from "react-google-maps";
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
 const styles = theme => ({
@@ -19,8 +20,8 @@ const styles = theme => ({
 const MapContainer = withScriptjs(
     withGoogleMap(props => (
         <GoogleMap
-            defaultZoom={9}
-            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            defaultZoom={10}
+            defaultCenter={{ lat: -33.309575, lng: 149.060731}}
             onClick={e => props.onMapClick(e)}
         >
             {props.marks.map((mark, index) => (
@@ -36,11 +37,20 @@ const MapContainer = withScriptjs(
 
 class Map extends Component {
     state = {
-        marks: []
+        marks: [{lat: -33.308849, lng: 149.010766},{lat: -33.3062539, lng: 148.9739605},{lat: -33.295481, lng: 148.839921},]
     };
 
+    getRandomInRange(from, to, fixed) {
+        return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+    }
+
     addMarker = e => {
-        this.setState({ marks: [...this.state.marks, e.latLng] });
+        var gen_lat = this.getRandomInRange(-33.0, -33.5, 3)
+        var gen_lon = this.getRandomInRange(149, 149.4, 3)
+        var newPos = {lat: gen_lat, lng: gen_lon}
+
+        this.setState({ marks: [...this.state.marks, ...[newPos] ] })
+
         console.log(this.state.marks)
     };
 
@@ -55,26 +65,29 @@ class Map extends Component {
         const { classes } = this.props;
         return (
             <div>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.formButton}
-              onClick={this.deleteMarkS}
-              type="submit">
-              Show My Readings
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.formButton}
-              onClick={this.deleteMarkS}
-              type="submit">
-              Add Location From Devices
-            </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.formButton}
+                  onClick={this.deleteMarkS}
+                  type="submit">
+                  Show My Readings
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  className={classes.formButton}
+                  onClick={this.addMarker}
+                  type="submit">
+                  Add Location From Devices
+                </Button>
+                <Typography variant="title" color="inherit" className={classes.flex}>
+                  The Markers below show the areas of clean water or contaminated areas around you
+                </Typography>
                 <MapContainer
                     googleMapURL="http://maps.googleapis.com/maps/api/js?key=AIzaSyDZPmheqXofqkjMcimKOvEkGJhxWyhuaCg"
                     loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `400px` }} />}
+                    containerElement={<div style={{ height: `500px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
                     onMapClick={this.addMarker}
                     marks={marks}
