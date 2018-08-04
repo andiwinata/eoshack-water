3090bd8501c9f527959ab0fb2028ae922c043bfe
Kogulan Ledanan <kogulan@isentia.com>
2018-08-04 19:38:38 +1000

    initial commit

--- quick_start.sh
+++ quick_start.sh
@@ -3,7 +3,7 @@
 # # make sure everything is clean and well setup
 ./first_time_setup.sh
 
-# # start blockchain and put in background
+# # start blockchain and put in background test
 ./start_eosio_docker.sh --nolog
 
 # start frontend react app
