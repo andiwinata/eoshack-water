ec3dbecbb09e1b65f5f306bd165b0135c90d53bb
Kogulan Ledanan <kogulan@isentia.com>
2018-08-05 08:18:00 +1000

    added dynamic markers from device

--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -5,30 +5,84 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const MapWithAMarker = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    <Marker
-      position={{ lat: -34.397, lng: 150.644 }}
-    />
-  </GoogleMap>
-));
+const icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+const styles = theme => ({
+  formButton: {
+    marginTop: theme.spacing.unit,
+    width: "100%",
+  }
+});
 
+const MapContainer = withScriptjs(
+    withGoogleMap(props => (
+        <GoogleMap
+            defaultZoom={9}
+            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            onClick={e => props.onMapClick(e)}
+        >
+            {props.marks.map((mark, index) => (
+                <Marker
+                  position={mark}
+                  key={index}
+                  icon={icon}
+                />
+            ))}
+        </GoogleMap>
+    ))
+);
 
 class Map extends Component {
-  render(){
-    return(
-      <MapWithAMarker
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZPmheqXofqkjMcimKOvEkGJhxWyhuaCg&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    );
-  }
-};
+    state = {
+        marks: []
+    };
 
-export default Map;
+    addMarker = e => {
+        this.setState({ marks: [...this.state.marks, e.latLng] });
+        console.log(this.state.marks)
+    };
+
+    deleteMarkS = () => {
+        this.setState({
+            marks: []
+        });
+    };
+
+    render() {
+        const { marks } = this.state;
+        const { classes } = this.props;
+        return (
+            <div>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.formButton}
+              onClick={this.deleteMarkS}
+              type="submit">
+              Add My Device
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.formButton}
+              onClick={this.deleteMarkS}
+              type="submit">
+              Load Locations
+            </Button>
+                <MapContainer
+                    googleMapURL="http://maps.googleapis.com/maps/api/js?key=AIzaSyDZPmheqXofqkjMcimKOvEkGJhxWyhuaCg"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                    onMapClick={this.addMarker}
+                    marks={marks}
+                />;
+            </div>
+        );
+    }
+}
+
+
+export default withStyles(styles)(Map);
